d0f2e44ead7786580321f8fe82530990fbebb1f1
chugachski <natesmith.copper@gmail.com>
2016-05-19 19:48:07 -0800

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,19 @@
-# Code your solution here!
+def run_guessing_game
+  response = "" # create response var so we have a condition for while loop
+  
+  while response != "exit"
+    puts "Guess a number between 1 and 6."
+    random_num = rand(1..6)
+    response = gets.chomp
+    
+    if response.to_i == random_num # have to convert string data to integer
+      puts "You guessed the correct number!"
+    else
+      puts "The computer guessed #{random_num}." 
+    end
+  end
+  
+  puts "Goodbye!"
+end
+
+
